976aebffc0dc2c0ee102a05d7bc5bbc7d365114b
Muhamad Adam 84 <muhamadadam20@gmail.com>
2020-05-06 14:43:05 +0700

    FIX

---
+++ app.js
@@ -0,0 +1,3 @@
+const mileage = require('./src/mileage')
+
+console.log(mileage("10:12:21", "12:00:00") + " KM")
---
+++ src/mileage.js
@@ -0,0 +1,41 @@
+function separate_time(_param) {
+    let time = _param.split(':')
+
+    return {
+        hour: parseInt(time[0]),
+        minute: parseInt(time[1]),
+        second: parseInt(time[2])
+    }
+}
+
+function toSecond(separated_time) {
+    return separated_time.hour * 3600 + separated_time.minute * 60 + separated_time.second
+}
+
+function toMinute(from_second) {
+    return from_second / 60
+}
+
+function mileage(_start, _end) {
+    let start = separate_time(_start)
+    let end = separate_time(_end)
+    let speed = 6
+    let total_distance = 0
+    
+    total_distance += speed*5*60
+    speed += 2        
+
+    let count_on_second = toSecond(end) - toSecond(start)
+    console.log(count_on_second)
+    if(count_on_second < 0) return false    
+
+    for(let a = (5*60); a < count_on_second; a++) {
+        if(toMinute(a) % 10 == 0) speed++
+
+        total_distance += speed //counter distance
+    }    
+
+    return total_distance / 1000
+}
+
+module.exports = mileage
