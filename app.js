4aabd496909dd547595d7f11da45fbbba8832aec
Muhamad Adam 84 <muhamadadam20@gmail.com>
2020-05-06 18:54:04 +0700

    Fix lagi hehe

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const mileageFix = require('./src/mileage-fix')
 
 console.log(mileage("10:12:21", "12:00:00") + " KM")
 console.log(mileageFix("10:12:21", "12:00:00") + " KM")
+
+// Keduanya bisa, sama-sama jalan, cuman buat readable, lebih baik yang mileageFix hehe (Reference: Code Mas Irul)
